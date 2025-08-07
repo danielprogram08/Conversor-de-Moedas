d6ff0f64c8d514f471311087cc3dfab579c8267b
danielprogram08 <danieldanielsilva08@gmail.com>
2025-08-07 00:02:56 -0300

    Botão de inverter valores concluído!

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
-let inputCoin = document.getElementById("input-coin");
 let inputValue = document.getElementById("input-value");
-let outputCoin = document.getElementById("output-coin");
 let outputValue = document.getElementById("output-value");
 
 // Invertendo os valores dos campos de entrada e saída;
 function invertValues() {
     let firstValue = inputValue.value;
     let secondValue = outputValue.value;
-    secondValue = firstValue;
-    firstValue = secondValue;
+    outputValue.value = firstValue;
+    inputValue.value = secondValue;
 }
+
