785f4d9512268cb1b4f6755b93c3a77e22650c26
danielprogram08 <danieldanielsilva08@gmail.com>
2025-08-18 23:57:42 -0300

    Botão de converter valores 100% funcional!

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let inputValue = document.getElementById("input-value");
 let outputValue = document.getElementById("output-value");
+let inputCoin = document.getElementById("input-coin");
+let outputCoin = document.getElementById("output-coin");
 
 // Invertendo os valores dos campos de entrada e saída;
 function invertValues() {
@@ -9,3 +11,18 @@ function invertValues() {
     inputValue.value = secondValue;
 }
 
+// Convertendo valores;
+async function ConvertValues() {
+    
+    const options = { method: 'GET', Headers: {accept: 'aapplication/json'}};
+    const GetUrl = `https://api.fastforex.io/convert?api_key=a0216e4e5b-36dbbbdc32-t14bb0&from=${inputCoin.value}&to=${outputCoin.value}&amount=${inputValue.value}`;
+
+    const response = await fetch(GetUrl, options);
+
+    if (!response.ok) { console.log('Erro ao converter valor! ' + response.status); }
+
+    const data = await response.json();
+    const result = data.result;
+    
+    outputValue.value = result[`${outputCoin.value}`];
+}
