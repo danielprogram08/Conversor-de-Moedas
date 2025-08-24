f3f7338cb7e8056dd71d0ceca6d97a84915945a1
danielprogram08 <danieldanielsilva08@gmail.com>
2025-08-24 00:45:38 -0300

    Conversor de Moedas 90% concluído!

--- script.js
+++ script.js
@@ -2,6 +2,12 @@ let inputValue = document.getElementById("input-value");
 let outputValue = document.getElementById("output-value");
 let inputCoin = document.getElementById("input-coin");
 let outputCoin = document.getElementById("output-coin");
+let titlebtn = document.getElementById('title-btn');
+let loadspinner = document.getElementById('loader');
+
+Alert();
+
+loadspinner.style.display = 'none';
 
 // Invertendo os valores dos campos de entrada e saída;
 function invertValues() {
@@ -13,16 +19,55 @@ function invertValues() {
 
 // Convertendo valores;
 async function ConvertValues() {
-    
+    titlebtn.style.display = 'none';
+    loadspinner.style.display = 'inline-block';
+
+    let value = parseFloat(inputValue.value);
     const options = { method: 'GET', Headers: {accept: 'aapplication/json'}};
-    const GetUrl = `https://api.fastforex.io/convert?api_key=a0216e4e5b-36dbbbdc32-t14bb0&from=${inputCoin.value}&to=${outputCoin.value}&amount=${inputValue.value}`;
+    const GetUrl = `https://api.fastforex.io/convert?api_key=a0216e4e5b-36dbbbdc32-t14bb0&from=${inputCoin.value}&to=${outputCoin.value}&amount=${value}`;
+
+    if (!value) {
+        alert(`Insira um número válido de entrada!`);
+        loadspinner.style.display = 'none';
+        titlebtn.style.display = 'block';
+    }
 
     const response = await fetch(GetUrl, options);
 
-    if (!response.ok) { console.log('Erro ao converter valor! ' + response.status); }
+    if (!response.ok) {
+        console.log('Erro ao converter valor! ' + response.status);
+    }
 
     const data = await response.json();
     const result = data.result;
     
     outputValue.value = result[`${outputCoin.value}`];
+
+    loadspinner.style.display = 'none';
+    titlebtn.style.display = 'block'
+}
+
+// Alerta;
+function Alert() {
+    const alert = document.createElement('div');
+
+    alert.className = 'alert';
+    alert.innerHTML = `
+    <p>ATENÇÃO!</p>
+    <p>Use '.' ao invés de ',' para a conversão correta do valor inserido!</p>`;
+
+    document.body.insertBefore(alert, document.body.firstChild);
+
+    setTimeout(() => {
+        alert.style.backgroundColor = 'rgba(0, 0, 0, 0.62)';
+        alert.style.color = 'rgba(255, 255, 255, 0.69)';
+    }, 1000);
+
+    setTimeout(() => {
+        alert.style.backgroundColor = 'rgba(0, 0, 0, 0)';
+        alert.style.color = 'rgba(255, 255, 255, 0)';
+        setTimeout(() => {
+            alert.remove();
+        }, 7000);
+    }, 6000);
 }
